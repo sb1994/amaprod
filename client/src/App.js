3e93dcd56af5cb78e81607c19e9527fd7e053c4f
Sean Boyle <sean94@gmail.com>
2020-08-30 20:24:15 +0100

    Editing ProductReviewForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Provider } from 'react-redux'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import axios from "axios";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import setUserToken from './utils/setUserToken'
+import setUserToken from "./utils/setUserToken";
 
-import jwt_decode from 'jwt-decode'
+import jwt_decode from "jwt-decode";
 
-import store from './store'
+import store from "./store";
 
-import Landing from './components/Landing/Landing'
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import Dashboard from './components/Auth/Dashboard'
-import DashboardAccount from './components/Auth/DashboardAccount'
-import ProductDetail from './components/Products/ProductDetail'
-import ProductList from './components/Products/ProductList'
+import Landing from "./components/Landing/Landing";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Dashboard from "./components/Auth/Dashboard";
+import DashboardAccount from "./components/Auth/DashboardAccount";
+import ProductDetail from "./components/Products/ProductDetail";
+import ProductList from "./components/Products/ProductList";
 // import DashboardAccount from './components/Auth/DashboardAccount'
 
-import Navigation from './components/Nav/Navigation'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { setLoggedUser } from './actions/userAuthActions'
+import Navigation from "./components/Nav/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { setLoggedUser } from "./actions/userAuthActions";
 
 if (localStorage.token) {
-  setUserToken(localStorage.token)
-  const decoded = jwt_decode(localStorage.token)
+  setUserToken(localStorage.token);
+  const decoded = jwt_decode(localStorage.token);
 
   // Set user and isAuthenticated
-  store.dispatch(setLoggedUser(decoded))
-  console.log(decoded)
+  store.dispatch(setLoggedUser(decoded));
+  console.log(decoded);
 }
 class App extends Component {
   render() {
@@ -38,25 +38,25 @@ class App extends Component {
           <Navigation />
           <Switch>
             {/* <Route exact path="/" component={Landing} /> */}
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/dashboard" component={Dashboard} />
             <Route
               exact
-              path='/products/:id/detail'
+              path="/products/:id/detail"
               component={ProductDetail}
             />
             {/* <Route exact path="/products" component={ProductList} /> */}
-            <Route exact path='/' component={ProductList} />
+            <Route exact path="/" component={ProductList} />
             <Route
               exact
-              path='/dashboard/account'
+              path="/dashboard/account"
               component={DashboardAccount}
             />
           </Switch>
         </Router>
       </Provider>
-    )
+    );
   }
 }
-export default App
+export default App;
--- client/src/components/Products/ProductDetail.js
+++ client/src/components/Products/ProductDetail.js
@@ -1,83 +1,86 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getSelectedProduct } from '../../actions/productActions'
-import ProductReviewForm from './ProductReviewForm'
-import ProductReviewList from './ProductReviewList'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSelectedProduct } from "../../actions/productActions";
+import ProductReviewForm from "./ProductReviewForm";
+import ProductReviewList from "./ProductReviewList";
 
 //ustils functions
-import { checkReviewFormRender } from '../../utils/functions'
+import { checkReviewFormRender } from "../../utils/functions";
 
 export class ProductDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = {}
+    this.state = {};
   }
 
   componentDidMount() {
-    this.props.getSelectedProduct(this.props.match.params.id)
+    this.props.getSelectedProduct(this.props.match.params.id);
   }
   render() {
     // console.log(this.props.product.product)
-    let { product } = this.props.product
-    let { isAuthenticated } = this.props.auth
-    let { user } = this.props.auth
+    let { product } = this.props.product;
+    let { isAuthenticated } = this.props.auth;
+    let { user } = this.props.auth;
     if (product === undefined) {
       return (
-        <div className='container'>
-          <div className='row'>
-            <div className='col-12'>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
               <p>Loading</p>
             </div>
           </div>
         </div>
-      )
+      );
     } else {
-      let { reviews } = product
-      console.log(reviews)
+      let { reviews } = product;
+      // console.log(reviews);
       if (reviews !== undefined) {
         // console.log(reviews)
         //checks if a user has already left a review so that
         //they can only leave one
-        let renderReviewForm = checkReviewFormRender(reviews, user._id)
+        // let renderReviewForm = checkReviewFormRender(reviews, user._id);
         // let renderReviewForm = reviews.includes((review) => {
         //   if (review.user._id !== user._id) {
         //     return true
         //   }
         // })
-        console.log(renderReviewForm)
+        // console.log(renderReviewForm);
       }
       return (
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-6 col-12 col-sm-12'>
-              <img src={product.image} style={{ height: '300px' }} />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-12 col-sm-12">
+              <img src={product.image} style={{ height: "300px" }} />
             </div>
-            <div className='col-md-6 col-12 col-sm-12'>
-              <div className='row'>
-                <div className='col-12'>
+            <div className="col-md-6 col-12 col-sm-12">
+              <div className="row">
+                <div className="col-12">
                   <h1>{product.name}</h1>
                   <p>{product.description}</p>
                 </div>
                 {isAuthenticated ? (
-                  <div className='col-12'>
-                    <button className='btn btn-primary'>Add to Cart</button>
+                  <div className="col-12">
+                    <button className="btn btn-primary">Add to Cart</button>
                   </div>
                 ) : null}
               </div>
             </div>
           </div>
           <hr />
-          <div className='row'>
-            <div className='col-12'>
-              <ProductReviewForm product_id={product.product_id} />
+          <div className="row">
+            <div className="col-12">
+              <ProductReviewForm
+                product_id={product.product_id}
+                reviews={reviews}
+              />
             </div>
-            <div className='col-12'>
+            <div className="col-12">
               <ProductReviewList reviews={reviews} />
             </div>
           </div>
         </div>
-      )
+      );
     }
   }
 }
@@ -85,8 +88,8 @@ export class ProductDetail extends Component {
 const mapStateToProps = (state) => ({
   auth: state.auth,
   product: state.product,
-})
+});
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, { getSelectedProduct })(ProductDetail)
+export default connect(mapStateToProps, { getSelectedProduct })(ProductDetail);
