28c15c2df390ed5c443d8d662654fbb35e0addd7
sb1994 <sean94@gmail.com>
2020-09-07 07:08:19 +0100

    Added custom style to UserCartItem

--- client/src/components/Auth/UserCartItem.js
+++ client/src/components/Auth/UserCartItem.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import { withRouter } from 'react-router-dom'
+import './user_cart_item.css'
 class UserCartItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hover: false,
+    }
+  }
+
+  handleShowProductDetail = () => {
+    let { id } = this.props.product
+    this.props.history.push(`/products/${id}/detail`)
+  }
   render() {
     let { product } = this.props
     return (
@@ -10,14 +23,20 @@ class UserCartItem extends Component {
           <div className='col-3'>
             <img
               src={product.product.image}
-              className='img-fluid'
+              className='img-fluid product-img'
               style={{ height: 100 }}
+              onClick={this.handleShowProductDetail}
             />
           </div>
           <div className='col-9'>
             <div className='row'>
               <div className='col-9'>
-                <p>{product.product.name}</p>
+                <p
+                  className='product-title'
+                  onClick={this.handleShowProductDetail}
+                >
+                  {product.product.name}
+                </p>
                 <p>Quatity: {product.quantity}</p>
               </div>
               <div className='col-3'>
@@ -36,4 +55,6 @@ const mapStateToProps = (state) => ({})
 
 const mapDispatchToProps = {}
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserCartItem)
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(UserCartItem)
+)
