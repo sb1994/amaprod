13e5fe52d77cad495844b6af728b60ac75f7774e
sb1994 <sean94@gmail.com>
2020-09-07 04:32:53 +0100

    Added util function to return Total Price of Items in cart

--- client/src/utils/functions.js
+++ client/src/utils/functions.js
@@ -13,15 +13,22 @@ export const checkReviewFormRender = (reviews, user_id) => {
 }
 export const returnTotalPrice = (cart) => {
   let totalPrice = 0
+  //gets the quantitys of the products
+  //and puts them into an array
   let quantites = cart.map((item) => {
     return item.quantity
   })
+
+  //gets the prices of the products
+  //and puts them into an array
   let prices = cart.map((item) => {
     return parseFloat(item.product.price)
   })
 
   // let totalPrice = 0
 
+  //get the total price of the cart by multiplying them all together
+  // with the quantitiy of the items
   for (let i = 0; i < quantites.length; i++) {
     totalPrice = totalPrice + quantites[i] * prices[i]
   }
