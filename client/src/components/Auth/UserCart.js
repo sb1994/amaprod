70331346d8e51ea244aed0b0c151dd7926898aad
Sean Boyle <sean94@gmail.com>
2020-09-07 02:35:23 +0100

    Adding UserCartItem to UserCart UI

--- client/src/components/Auth/UserCart.js
+++ client/src/components/Auth/UserCart.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import UserCartItem from "./UserCartItem";
 
 export class UserCart extends Component {
   render() {
+    let renderCartItems = this.props.auth.cart.map((item, index) => {
+      return <UserCartItem key={index} product={item} />;
+    });
     return (
       <div className="container">
         <h1>User Cart</h1>
+        <hr />
+        <div className="row">{renderCartItems}</div>
       </div>
     );
   }
---
+++ client/src/components/Auth/UserCartItem.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UserCartItem extends Component {
+  render() {
+    let { product } = this.props;
+    return (
+      <div className="col-12">
+        <div className="row">
+          <div className="col-3">
+            <img
+              src={product.product.image}
+              className="img-fluid"
+              style={{ height: 100 }}
+            />
+          </div>
+          <div className="col-9">
+            <div className="row">
+              <div className="col-9">
+                <p>{product.product.name}</p>
+              </div>
+              <div className="col-3">
+                <p>Price: {product.product.price}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserCartItem);
